1927eee194bf167fd424bc5f9df654163c48887b
miguel <miguelangelgarciarueda@gmail.com>
2022-03-25 08:36:51 +0100

    mi primer commit

---
+++ src/prueba/Person.java
@@ -0,0 +1,30 @@
+package prueba;
+
+/**
+ * TODO mgarci23 This type ...
+ *
+ */
+public class Person {
+
+  private int age;
+
+  public Person() {
+
+    this.age = 0;
+  }
+
+  public Person(int edad) {
+
+    this.age = edad;
+  }
+
+  public int getAge() {
+
+    return this.age;
+  }
+
+  public String decirHola() {
+
+    return "hola";
+  }
+}
---
+++ src/prueba/test.java
@@ -0,0 +1,32 @@
+package prueba;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * TODO mgarci23 This type ...
+ *
+ */
+public class test {
+
+  /**
+   * @param args
+   */
+
+  @Test
+  public void demonstrate() {
+
+    Person señor = new Person(4);
+
+    assertEquals("hola", señor.decirHola());
+    assertEquals(4, señor.getAge());
+  }
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+    System.out.println("hello world");
+  }
+
+}
